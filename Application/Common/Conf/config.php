61f77d56d38839330e64a0ca0b881557bb340724
Shangzikun <290917115@qq.com>
2018-12-04 10:40:14 +0800

    更改数据库配置文件

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,4 +1,15 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
+	//数据库配置信息-----------------
+	'DB_TYPE'   => 'mysql',//数据库类型
+	'DB_HOST'   => '127.0.0.1',//服务器地址
+	'DB_NAME'	=> 'qinghuameiyuan',//数据库名字
+	'DB_USER'	=> 'root',//用户名
+	'DB_PWD'	=> 'root',//密码
+	'DB_PERFIX' => 'tp_'//表前缀
+	'DB_CHARSET'=> 'utf8'//字符集
+	//网站配置------------------------
+	'NOW_PATH'	=> 'http://qh.com',
+	'QQ'		=> '290917115',
+	
 );
